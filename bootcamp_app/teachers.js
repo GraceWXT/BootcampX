5eb0215e34dc6140ec2f6163441ae93d388d058d
Grace Wang <wangxiaotian2012@gmail.com>
2022-04-07 03:13:48 +0000

    Practiced node-postgres pool.query with this database.

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,18 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-const limit = process.argv[3];
 pool.query(`
-SELECT students.id, students.name, cohorts.name AS cohort
-FROM students
+SELECT DISTINCT teachers.name, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE $2
-LIMIT $1;
-`, [limit || 3, `%${cohortName}%`])
+WHERE cohorts.name = $1
+ORDER BY teachers.name;
+`, [cohortName])
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  res.rows.forEach(teacher => {
+    console.log(`${cohortName}: ${teacher.name}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
